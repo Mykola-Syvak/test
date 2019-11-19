d5367ce7b5e784c53aa1c70c5b38265919e9c512
Kolya <mykola.syvak@gmail.com>
2019-11-19 14:02:32 +0200

    Это наш восьмой комит

--- 2/script-8.js
+++ 2/script-8.js
@@ -1,29 +1,39 @@
 'use strict';
 /*jshint esversion: 6 */
-let money = parseInt(prompt('Ваш бюджет на месяц?', 0));
-let time = new Date(prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD'));
-let asapBuy;
-let asapBuySumm;
+let money = +prompt('Ваш бюджет на месяц?', 0);
+let time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD');
 
 let appData = {
-    budget: parseInt(money),
+    budget: money,
     timeData: time,
-    expenses: {
-      asapBuy : asapBuySumm
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
 for (let i = 0; i < 2; i++) {
-    asapBuy = prompt('Введите обязательную статью расходов в этом месяце', 'холодильник');
-    asapBuySumm = parseInt(prompt('Во сколько обойдется?', 1000));
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && ((typeof(a)) != null) && ((typeof(b)) != null)
+    && a != '' && b != '' && a.length  < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        
+    }
 };
 
+appData.moneyPerDay = appData.budget / 30;
 
-if (typeof(asapBuy) === 'string' && asapBuy != '' && asapBuySumm != '') {
-    alert(appData.budget/30);
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Высокий уровень достатка");
 } else {
-    alert("Вы ввели недействительное значение");
+    console.log ("Произошла ошибка");
 }
--- 2/script.js
+++ 2/script.js
@@ -1,7 +1,8 @@
 'use strict';
+/*jshint esversion: 6 */
 
-let num = 50;
 
+/*
 if(num < 49) {
     console.log('Неверно');
 } else if (num > 100) {
@@ -28,4 +29,43 @@ switch (num) {
     default:
         console.log('Что-то пошло не так');
         break;    
+}*/
+
+let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+/*do {
+    console.log(num);
+    num++;
 }
+while (num < 55)
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
+
+let x = 5;
+console.log( ++x );
+
+let z = [ ] + false - null + true;
+console.log(z);*/
+
+console.log("0"[0]);
+
+console.log(( null || 2 && 3 || 4 ));
+
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+
+console.log(+Infinity);
+
+console.log( "".codePointAt(0) );
+console.log( "яблоко".codePointAt(0) );
+
+console.log(0 || "" || 2 || undefined || true || false);
