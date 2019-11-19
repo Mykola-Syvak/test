21d87ab5696f23e0521fcf7d98f8699d06fe1115
Kolya <mykola.syvak@gmail.com>
2019-11-19 14:14:02 +0200

    Задание 9.2 готово

--- 2/script-8.js
+++ 2/script-8.js
@@ -20,9 +20,37 @@ for (let i = 0; i < 2; i++) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
-        
+        alert("Вы ввели ошибочные данные");
     }
 };
+
+/*let i = 0;
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && ((typeof(a)) != null) && ((typeof(b)) != null)
+    && a != '' && b != '' && a.length  < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+        i++;
+    } else {
+        alert("Вы ввели ошибочные данные");
+    }
+}*/
+
+/*let i = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && ((typeof(a)) != null) && ((typeof(b)) != null)
+    && a != '' && b != '' && a.length  < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+        i++;
+    } else {
+        alert("Вы ввели ошибочные данные");
+    }
+} while (i < 2);*/
 
 appData.moneyPerDay = appData.budget / 30;
 
