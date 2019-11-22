d3f3fb27c3ee6cae45158b2eafb6842d9dfd6429
Kolya <mykola.syvak@gmail.com>
2019-11-22 17:15:26 +0200

    делаем задание 17-1

---
+++ 2/17-1/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+/*jshint esversion: 6 */
+/*jshint devel:true */
+
+// let box = document.getElementById('box'),
+//     btn = document.getElementsByTagName('button'),
+//     circle = document.getElementsByClassName('circle'),
+//     heart = document.querySelectorAll('.heart'),
+//     oneHeart = document.querySelector('.heart'),
+//     wrapper = document.querySelector('.wrapper');
+
+let menuUl = document.querySelector('.menu'),
+    menuItems = document.querySelector('.menu-item');
+       
+    menuUl.removeChild(menuItems[1]);
+    //***ARBAYTEN***!!! head = document.querySelector('header');
+//***ARBAYTEN***!!!document.body.removeChild(head);
+
+
