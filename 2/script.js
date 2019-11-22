c6d4d0859dbe65e150e8aa152d8171a4ed52e644
Kolya <mykola.syvak@gmail.com>
2019-11-22 14:05:45 +0200

    теория 17 закончена

--- 2/script.js
+++ 2/script.js
@@ -179,24 +179,66 @@ console.log(Object.keys(options).length);*/
 
 // console.log(i);
 
-let arr = [1, 15, 4],
-     i = arr.sort(compareNum);
+// let arr = [1, 15, 4],
+//      i = arr.sort(compareNum);
 
-     function compareNum(a,b) {
-    return a-b;
-}
+//      function compareNum(a,b) {
+//     return a-b;
+// }
 
- console.log(arr);
+//  console.log(arr);
 
- let soldier = {
-     health: 400,
-     armor: 100
- };
+//  let soldier = {
+//      health: 400,
+//      armor: 100
+//  };
 
- let john = {
-     health: 100
- };
+//  let john = {
+//      health: 100
+//  };
 
- john.__proto__ = soldier;
- console.log(john);
- console.log(john.armor);
+//  john.__proto__ = soldier;
+//  console.log(john);
+//  console.log(john.armor);
+// ****** 2.16 lesson ******
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '100%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function (item, i, hearts) {
+//     item.style.backgroundColor = 'blue';
+// });
+
+// ****** 2.17 lesson ******
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+// div.innerHTML = '<h1>Hello World!</h1>';
+div.textContent = 'Hello World!';
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
+
+console.log(div);
