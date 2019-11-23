cb9765cfe7750759e2cf34aeb5db180821ce681d
Kolya Home <mykola.syvak@gmail.com>
2019-11-23 16:41:53 +0200

    Задание 17-1 готово

--- 2/17-1/js/script.js
+++ 2/17-1/js/script.js
@@ -11,14 +11,35 @@
 
 let menuUl = document.querySelector('.menu'),
     menuItems = document.querySelector('.menu-item'),
-    list = document.getElementsByTagName('li');
+    list = document.getElementsByTagName('li'),
+    body = document.getElementsByTagName('body'),
+    div = document.getElementsByTagName('div'),
+    li = document.createElement('li'),
+    title = document.querySelector('.title'),
+    column = document.querySelector('.column'),
+    promptUser = document.querySelector('#prompt'),
+    adv = document.querySelector('.adv');
        
-    console.log(list[1]);
-
     let replacedNode = menuUl.replaceChild(list[0], list[1]); // третий был удален и заменен
     menuUl.insertBefore(replacedNode, list[2]);
+
+    li.classList.add('menu-item');
+    li.textContent = 'Пятый пункт';
+    menuUl.insertBefore(li, list[4]); //добавили 5й пункт
+
+    document.body.style.background = "url('img/apple_true.jpg')"; //меняем картинку бекграунда
+    title.textContent = "Мы продаем только подлинную технику Apple"; //меняем слоган
+    div[1].removeChild(adv); //удаляем рекламу
+
+    let question;
+    function userQuestion() {
+        question = prompt('Как вы относитесь к технике Apple?',''); // спрашиваем у пользователя
+        promptUser.textContent = question; // записываем ответ в соответствующее поле
+    };
+
+    setTimeout('userQuestion()', 500);
+
+
     
-    //***ARBAYTEN***!!! head = document.querySelector('header');
-//***ARBAYTEN***!!!document.body.removeChild(head);
 
 
