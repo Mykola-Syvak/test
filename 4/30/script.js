95f78f8ecc8a906605c0501f4a48131ddf5a2afe
Kolya <mykola.syvak@gmail.com>
2019-12-04 15:01:27 +0200

    Теория 30 закончена

---
+++ 4/30/script.js
@@ -0,0 +1,15 @@
+'use strict';
+/*jshint esversion: 6 */
+/*jshint devel:true */
+
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
