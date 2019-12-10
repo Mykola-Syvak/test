e174b459e6f8ebe0cb4956a26f4b851b40a36acb
Kolya <mykola.syvak@gmail.com>
2019-12-10 13:38:35 +0200

    Задание 32-1-1 закончено

--- 3/yoga/js/script.js
+++ 3/yoga/js/script.js
@@ -125,45 +125,61 @@ window.addEventListener('DOMContentLoaded', function() {
     let form = document.querySelector('.main-form'),
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
-
+        
         statusMessage.classList.add('status');
     
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+    
+    function sendForm(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+            let formData = new FormData(form);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        request.setRequestHeader('Content-type', 'application/json; charser=utf-8');
+            function postData(data) {
 
-        let formData = new FormData(form);
+                return new Promise(function(resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    //request.setRequestHeader('Content-type', 'application/json; charser=utf-8');
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    // let obj = {};
+                    // formData.forEach(function(value, key) {
+                    //     obj[key] = value;
+                    // });
+                    // let json = JSON.stringify(obj); //
+                
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) { 
+                            resolve()
+                        } else if(request.readyState === 4) {
+                            if (request.status == 200 && request.status < 3) {
+                                resolve()
+                            }
+                            else {
+                            reject()
+                            }
+                        }
+                    }
 
-        console.log(formData);
+                    request.send(data);
+                    
+                });
+            };
 
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
-        
-        request.send(json);
-
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if(request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
             }
+
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
         });
-
-            for (let i = 0; i < input.length; i++) {
-                input[i].value = '';
-            }
-    });
-
+    }
+    sendForm(form);
     // Contact Form --> server
 
     let contactForm = document.querySelector('#form'),
